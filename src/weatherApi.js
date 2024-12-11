b2d69b5f1a24295f9764bdc706d8c72192e5db6e
GooseIF <168380059+GooseIF@users.noreply.github.com>
2024-12-11 13:21:15 +0700

    Delete src directory

--- src/weatherApi.js
+++
@@ -1,9 +0,0 @@
-const key = "5d0b38825ba9e80dee5e6947e69e6ef4";
-
-export default function fetchWeather(lon, lat) {
-  const srcUrl = "https://api.openweathermap.org/data/2.5/weather?";
-
-  const url = `${srcUrl}lat=${lat}&lon=${lon}&units=metric&appid=${key}`;
-
-  return fetch(url);
-}
